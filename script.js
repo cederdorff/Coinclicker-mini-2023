630dd8a5615c91b00e82db0e01e834c46ffa0834
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-02-28 19:56:02 +0100

    Reset to sounds and start screen

--- script.js
+++ script.js
@@ -8,47 +8,15 @@ let lives = 0;
 function ready() {
     console.log("JavaScript ready!");
     document.querySelector("#btn_start").addEventListener("click", startGame);
-    document.querySelector("#btn_restart").addEventListener("click", startGame);
-    document.querySelector("#btn_go_to_start").addEventListener("click", showStartScreen);
-}
-
-function showGameScreen() {
-    // Skjul startskærm, game over og level complete
-    document.querySelector("#start").classList.add("hidden");
-    document.querySelector("#game_over").classList.add("hidden");
-    document.querySelector("#level_complete").classList.add("hidden");
-}
-
-function showStartScreen() {
-    // fjern hidden fra startskærm og tilføj til game over og level complete
-    document.querySelector("#start").classList.remove("hidden");
-    document.querySelector("#game_over").classList.add("hidden");
-    document.querySelector("#level_complete").classList.add("hidden");
-}
-
-function resetLives() {
-    // sæt lives til 3
-    lives = 3;
-    //nulstil visning af liv (hjerte vi ser)
-    document.querySelector("#heart1").classList.remove("broken_heart");
-    document.querySelector("#heart2").classList.remove("broken_heart");
-    document.querySelector("#heart3").classList.remove("broken_heart");
-    document.querySelector("#heart1").classList.add("active_heart");
-    document.querySelector("#heart2").classList.add("active_heart");
-    document.querySelector("#heart3").classList.add("active_heart");
-}
-
-function resetPoints() {
-    // nulstil point
-    points = 0;
-    // nulstil vising af point
-    displayPoints();
 }
 
 function startGame() {
-    resetLives();
-    resetPoints();
-    showGameScreen();
+    // nulstil point og liv
+    points = 0;
+    lives = 3;
+
+    // skjul startskærm
+    document.querySelector("#start").classList.add("hidden");
 
     // Start baggrundsmusik
     document.querySelector("#sound_dreams").play();
